52c46755ca49d91d81cc09c8abec21dc90f1607b
adamjz <jinjie_zhou@outlook.com>
2018-04-23 14:04:49 +0800

    淘宝爬虫

--- taobao_spider.py
+++ taobao_spider.py
@@ -11,6 +11,9 @@ class taobao():
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}
         self.conn_mysql = pymysql.connect(host="127.0.0.1", user='root', password='root', db='tb')
 
+    """
+	@ 获取商品的url、名称、显示价格和月销售量
+    """
     def get_info(self, page_url, urls, titles, prices, sales):
         response = self.session.get(page_url, headers=self.ua, verify=False)
         data = response.content.decode('utf-8')
@@ -34,6 +37,9 @@ class taobao():
             d = re.compile('([0-9]\d*)').findall(l)[0]
             sales.append(d)
 
+    """
+	@ 将获取到的信息写入数据库
+    """
     def insert_mysql(self, title, url, price, sale):
         sql = "insert into goods(title,url,price,sale) values('" + title + "','" + url + "','" + price + "','" + sale + "')"
         print(sql)
@@ -42,8 +48,10 @@ class taobao():
 
 
 if __name__ == '__main__':
-    name = quote('零食')
+	keyword = input("请输入搜索的商品关键字：")
+    name = quote(keyword)
     a = taobao()
+    # 循环搜索100页
     for i in range(0, 100):
         page_url = 'https://s.taobao.com/search?q=%s&s=%d' % (name, i * 44)
         urls = []
